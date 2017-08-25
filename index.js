6746d30fd5f852c042f9767a7ab275f061b62aee
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-08-25 15:58:16 +0200

    testing devices

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
+const devices = require('puppeteer/DeviceDescriptors')
+const path = require('path')
+
+const URL = 'https://www.extrategy.net/en/values'
+
+const run = async (browser, device) => {
+  const page = await browser.newPage()
+  const imagePath = path.join(__dirname, 'results', `${device.name}.png`)
+
+  try {
+    await page.emulate(device)
+    await page.goto(URL)
+    await page.click('.list_type11__item__wrapper__link')
+    await page.waitForNavigation({waitFor: 'networkidle'})
+    await page.screenshot({path: imagePath})
+  } catch (e) {
+    console.error(`Error in testing ${device.name}: ${e.message}`)
+  }
+
+  return page.close()
+}
 
 (async () => {
-  const URL = 'https://www.extrategy.net/'
-
   const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto(URL)
-  await page.click('.l-header__site_navigation__menu_primary__item:nth-child(3)')
-  await page.waitForNavigation({waitFor: 'networkidle'})
-  await page.click('.list_type11.row li:nth-child(5)')
-  await page.waitForNavigation({waitFor: 'networkidle'})
-  await page.screenshot({path: 'example.png'})
-
-  browser.close()
+  for (let index = 0; index < devices.length; index++) {
+    const device = devices[index]
+    console.log(`Testing ${device.name}`)
+    await run(browser, device)
+  }
+  return browser.close()
 })()
